c4cedef78c72667cbe4b7ccee0b879b34be02385
awanama <awanama@awanama.org>
2023-03-03 08:18:17 +0700

    create get request

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require("express");
+const path = require("path")
 
 const app = express();
 const PORT = 8080;
+
+app.get("/", (req, res) => {
+  // res.json({
+  //   msg: "welcome to coffee shop"
+  // })
+  res.status(200).sendFile(path.join(__dirname, "/src/html/welcome.html"))
+})
 
 app.listen(PORT, () => {
   console.log(`server is running at port ${PORT}`);
