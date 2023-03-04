5e306d8115a9b6c14723eb3034051a79f7fe2ca5
wyakaga <123617005+wyakaga@users.noreply.github.com>
2023-03-04 13:22:41 +0700

    add sql join

--- src/models/users.model.js
+++ src/models/users.model.js
@@ -2,16 +2,17 @@ const db = require("../configs/postgre");
 
 const getUsers = () => {
 	return new Promise((resolve, reject) => {
-		db.query(
-			"select id, email, display_name, birth_date from users order by id asc",
-			(err, result) => {
-				if (err) {
-					reject(err);
-					return;
-				}
-				resolve(result);
+		let sql =
+		`select u.id, u."email", u."password", u."phone_number", u."address", u."display_name",
+		u."first_name", u."last_name", u."birth_date", u."gender", r."name" as "role_name"
+		from users u join roles r on u.role_id = r.id ORDER BY id ASC`;
+		db.query(sql, (err, result) => {
+			if (err) {
+				reject(err);
+				return;
 			}
-		);
+			resolve(result);
+		});
 	});
 };
 
