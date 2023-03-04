87aa53f7a718049301b249c0396cf28d596c9941
wyakaga <123617005+wyakaga@users.noreply.github.com>
2023-03-04 15:16:28 +0700

    add limit to users

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,19 +1,20 @@
 const usersModel = require("../models/users.model");
 
-const getUsers = (req, res) => {
-	usersModel
-		.getUsers()
-		.then((result) => {
-			res.status(200).json({
-				data: result.rows,
-			});
-		})
-		.catch((error) => {
-			console.log(error.message);
-			res.status(500).json({
-				msg: "Internal Server Error",
-			});
+const getUsers = async (req, res) => {
+	try {
+		const { limit } = req.query;
+
+		const result = await usersModel.getUsers(limit);
+		console.log(limit);
+		res.status(200).json({
+			data: result.rows,
 		});
+	} catch (error) {
+		console.log(error.message);
+		res.status(500).json({
+			msg: "Internal Server Error",
+		});
+	}
 };
 
 module.exports = {
--- src/models/users.model.js
+++ src/models/users.model.js
@@ -1,11 +1,11 @@
 const db = require("../configs/postgre");
 
-const getUsers = () => {
+const getUsers = (limit) => {
 	return new Promise((resolve, reject) => {
 		let sql =
 		`select u.id, u."email", u."password", u."phone_number", u."address", u."display_name",
 		u."first_name", u."last_name", u."birth_date", u."gender", r."name" as "role_name"
-		from users u join roles r on u.role_id = r.id ORDER BY id ASC`;
+		from users u join roles r on u.role_id = r.id ORDER BY id ASC LIMIT ${limit}`;
 		db.query(sql, (err, result) => {
 			if (err) {
 				reject(err);
