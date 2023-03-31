e0eaa39fde68ab0fcec0cf4ce16eae6bb3beb81b
wyakaga <123617005+wyakaga@users.noreply.github.com>
2023-03-31 09:02:59 +0700

    test

--- index.js
+++ index.js
@@ -1,15 +1,41 @@
-require("dotenv").config();
+// require("dotenv").config();
 
+// const express = require("express");
+// const cors = require("cors");
+// const morgan = require("morgan");
+// const mongoose = require("mongoose");
+// const mainRouter = require("./src/routers");
+
+// const app = express();
+// const PORT = process.env.SERVER_PORT || 8080;
+// const mongoUrl = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PWD}@${process.env.MONGO_HOST}/?retryWrites=true&w=majority`;
+
+// app.use(cors());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(express.json());
+// app.use(express.static("public"));
+
+// app.use(morgan("combined"));
+
+// app.use(mainRouter);
+
+// mongoose
+// 	.connect(mongoUrl)
+// 	.then(() => {
+// 		console.log("MongoDB Connected");
+// 		app.listen(PORT, () => {
+// 			console.log(`server is running at port ${PORT}`);
+// 		});
+// 	})
+// 	.catch((err) => console.log(err));
+
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const morgan = require("morgan");
-const mongoose = require("mongoose");
 const mainRouter = require("./src/routers");
-
+const morgan = require("morgan");
 const app = express();
 const PORT = process.env.SERVER_PORT || 8080;
-const mongoUrl = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PWD}@${process.env.MONGO_HOST}/?retryWrites=true&w=majority`;
-
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -18,13 +44,6 @@ app.use(express.static("public"));
 app.use(morgan("combined"));
 
 app.use(mainRouter);
-
-mongoose
-	.connect(mongoUrl)
-	.then(() => {
-		console.log("MongoDB Connected");
-		app.listen(PORT, () => {
-			console.log(`server is running at port ${PORT}`);
-		});
-	})
-	.catch((err) => console.log(err));
+app.listen(PORT, () => {
+	console.log(`server is running at port ${PORT}`);
+});
